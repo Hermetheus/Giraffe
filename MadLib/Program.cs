9368838feeaeec56cd2686f0613bd585c04c1921
Allan <35552120+Hermetheus@users.noreply.github.com>
2018-11-26 11:46:21 -0600

    MadLib

---
+++ MadLib/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, person;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a person: ");
+            person = Console.ReadLine();
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + person);
+            Console.ReadLine();
+        }
+    }
+}
